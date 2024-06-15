92167e1e4e50282bc74ac788853babe041fe80ac
ABDUL MAJEED ABDALLAH <aa33@williams.edu>
2024-06-15 17:15:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <p className='note'>ClimaSense by Majeed®</p>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
